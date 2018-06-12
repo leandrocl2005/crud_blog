aee9ab3b0355cc6b23cff73a4c8698e3536cc8de
Leandro <leandrocl2005@yahoo.com.br>
2018-06-12 14:28:39 -0300

    without secretkey

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from .secret_info import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')cyi)lc63%-$p=h+y_c1ztz-##%5&_1dp#9#-8&9^m%-n0a4+p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
