d8131feff41a17fc287e1a5e60aa2b7812e1aa05
Leandro <leandrocl2005@yahoo.com.br>
2018-06-13 14:03:32 -0300

    Html jiraya

--- posts/views.py
+++ posts/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request,'posts_templates/posts_list.html',{'posts': posts})
+    return render(request,'home/index.html',{'posts': posts})
 
 @login_required
 def post_create(request):
