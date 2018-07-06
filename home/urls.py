5d7e6e61f8ed9ee1c233b9d9ad5ff611ca909fba
Leandro <leandrocl2005@yahoo.com.br>
2018-07-06 08:49:10 -0300

    dmat

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, contato, plot, dmat
 
 urlpatterns = [
+    path('contato', contato, name='contato'),
     path('', home, name='home'),
+    path('plot', plot, name='plot'),
+    path('dmat', dmat, name='dmat'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/index.html', {'iterators': [0,1,2,3,4,5,6,7]})
+    return render(request, 'home/index.html')
+
+def contato(request):
+    return render(request, 'home/contato.html')
+
+def plot(request):
+    names = ['A', 'B', 'C']
+    prices = [100,300,200]
+
+    context = {
+        'names': json.dumps(names),
+        'prices': json.dumps(prices),
+    }
+    return render(request, 'home/plot.html', context)
+
+def dmat(request):
+	return render(request, 'dmat/score.html')
